9440b69c8bb09b19438453f1956633ab09a1d4a1
rkdehdrn <rkdehdrn9@naver.com>
2023-06-13 11:44:37 +0900

    java2

---
+++ src/variable/Testclass01.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class Testclass01 {
+
+	public static void main(String[] args) {
+		/*
+		 변수
+		 -데이터를 저장하기 위한 공간
+		 변수 아룸 명명 규칙
+		 -첫 문자는 소문자로 생성한다
+		 -합성어의 첫 문자는 대문자로 생성
+		 -예약어는 사용할 수 없다.
+		 자료형
+		 -정수 : short, int. (2,4byte)
+		 -실수 : float, double (4,8)
+		 -문자 : byte. char (1,2)
+		 -논리형 : boolean(1). true(참) 또는 false(거짓) 만 저장
+		 */
+		 int age =30;
+		 double weigh = 55.0, height = 170.3;
+		 //double height 170.3
+		 System.out.println("나이는"+age+"살입니다");
+		 System.out.println("몸무게는"+weigh +"kg입니다");
+		 System.out.println("키는"+height+"cm입니다");
+		 System.out.println("키는"+height+"   살살입니다");
+		 
+		
+		
+		
+	}
+
+}
